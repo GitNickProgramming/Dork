cbce6f3c47e307aca9433eac217056713f36a838
Zach <zachoryanguiano@gmail.com>
2019-06-03 12:19:34 -0600

    Commit

--- dork/__init__.py
+++ dork/__init__.py
@@ -4,5 +4,5 @@
 
 __version__ = '0.1.0'
 __author__ = ", ".join([
-    "Peter Nielson, ", "David Dews, ", "Larsen James Close, ", 
+    "Peter Nielson, ", "David Dews, ", "Larsen James Close, ", "Larsen James Close, ",
 ])
