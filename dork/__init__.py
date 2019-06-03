3c7812c52aae3e432099d0fb3a0d40c0e0e6b8a3
Nicholas <ngagliar@msudenver.edu>
2019-06-03 14:08:14 -0600

    Added name to Developers

--- dork/__init__.py
+++ dork/__init__.py
@@ -4,5 +4,5 @@
 
 __version__ = '0.1.0'
 __author__ = ", ".join([
-    "Peter Nielson, ", "David Dews, ", "Larsen James Close, ", "Nicole Beck, ", "Zachory Anguiano, ", "Devon DeJohn, ", "Fernando Babonoyaba"
+    "Peter Nielson, ", "David Dews, ", "Larsen James Close, ", "Nicole Beck, ", "Zachory Anguiano, ", "Devon DeJohn, ", "Fernando Babonoyaba", "Nick Gagliardi"
 ])
