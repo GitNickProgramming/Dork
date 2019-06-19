f9f546a12d761f6c592cf94c3a25d11746506288
Larsen Close <closelj@gmail.com>
2019-06-19 13:30:10 -0600

    more style fixes

--- dork/map_generator.py
+++ dork/map_generator.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 
 CARDINALS = ["north", "east", "south", "west"]
 
+nodes = []
+edges = []
+
 
 def load_data(file_name_and_path = "./dork/dork.yml"):
     """Loads data from yaml file into data"""
