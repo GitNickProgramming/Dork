50b4308871b84a370c3a714f1480e104402445fe
david dews <ddews1@msudenver.edu>
2019-06-18 20:31:27 -0600

    maybe lint will smile upon me now!

--- tests/test_dork_command_parser.py
+++ tests/test_dork_command_parser.py
@@ -3,34 +3,46 @@ import map_parser.command_parser as _mp
 import dork.types as _tp
 import tests.daves_mock
 
+
 def test_hello_world():
     """Testing the hello world function of parser """
     assert _mp.get_hello_world()[0] == "hello, world!", "Hello world method failed"
+
 
 def test_goodbye_world():
     """Testing the goodbye function of parser """
     assert _mp.get_goodbye_world()[0] == "goodbye, world!", "Goodby world method failed"
 
+
 def test_show_help():
     """Testing the help method of parser """
     assert _mp.get_show_help()[0] == "try typing 'say hello'"
+
 
 def test_player_instance():
     """Testing the instantiation of Player class """
     player = _mp.create_player()
     assert player is not None, "Typing error, Player is None"
-    assert isinstance(player ,_tp.Player), "Typing error, player is not Player class"
+    assert isinstance(player, _tp.Player), """Typing error,
+    player is not Player class"""
+
 
 def test_evaluate():
     """Testing the eval function of Repl """
-    assert _mp.evaluate("say hello")[0] == "hello, world!", "Hello world evalutation failed"
-    assert _mp.evaluate("")[0] == "Unknown Command", "Input Type Handling Error, empty string"
-    assert _mp.evaluate(None)[0] == "Unknown Command", "Input Mismatch, unable to handle"
-    assert _mp.evaluate("say goodbye")[0] == "goodbye, world!", "Goodbye world evaluation failed"
-    assert _mp.evaluate("help say")[0] == "try typing 'say hello'", "help say command failed"
+    assert _mp.evaluate("say hello")[0] == "hello, world!", """Hello world"
+    evalutation failed"""
+    assert _mp.evaluate("")[0] == "Unknown Command", """Input Type Handling"
+    Error, empty string"""
+    assert _mp.evaluate(None)[0] == "Unknown Command", """Input Mismatch,"
+    unable to handle"""
+    assert _mp.evaluate("say goodbye")[0] == "goodbye, world!", """Goodbye world
+    evaluation failed"""
+    assert _mp.evaluate("help say")[0] == "try typing 'say hello'", """help say
+    command failed"""
+
 def test_read():
     """Testing the read function of Repl """
-    inp = tests.daves_mock.mock_input()
+    inp = tests.daves_mock.MockInput()
 
     inp.change_input("a string")
     _mp.input = inp.make_input
