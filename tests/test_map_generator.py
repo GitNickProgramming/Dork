0735726ed2fa4568fc799208a47981b99b1a5e70
Larsen Close <closelj@gmail.com>
2019-06-18 22:05:57 -0600

    fixed pep8 stuff

--- tests/test_map_generator.py
+++ tests/test_map_generator.py
@@ -1,59 +1,82 @@
+# -*- coding: utf-8 -*-
+"""Basic tests for the dork map generator
+"""
 
 import map_parser.map_generator as map
-import networkx as nx 
+import networkx as nx
 import os
+from tests.utils import is_a
 
 def test_map_generator_exists():
+    """Test if the map generator has a main"""
     assert "main" in vars(map)
 
+
 def test_file_path():
+    """Test File Path"""
     assert os.path.isfile('jabba') == False
     assert os.path.isfile('./map_parser/yaml/dork.yml') == True
 
 def test_cardinals():
-    assert type(map.CARDINALS) is list
+    """ Test Cardinals"""
+    is_a(map.CARDINALS, list)
     assert map.CARDINALS == ['north', 'east', 'south', 'west']
 
+
 def test_nodes():
+    """Test Nodes"""
     assert map.nodes is not None
-    assert type(map.nodes) is list
-    
+    is_a(map.nodes, list)
+
 
 def test_edges():
+    """Test Edges"""
     assert map.edges is not None
-    assert type(map.edges) is list
+    is_a(map.edges, list)
 
-    
+
 def test_functions_exist():
+    """Test Functions exist"""
     assert "_load_data" in vars(map)
     assert "convert_dict_edges" in vars(map)
     assert "convert_dict_nodes" in vars(map)
     assert "_check_data" in vars(map)
-    assert "_generate_map" in vars(map) 
-   
+    assert "_generate_map" in vars(map)
+
+
 def test_load_data():
-    assert map._load_data() != None
+    """Test Load Data"""
+    assert map._load_data() is not None
     assert map._load_data() != ""
-    assert type(map._load_data()) is dict
+    is_a(map._load_data(), dict)
+
 
 def test_convert_dict_edges():
+    """Test Convert Dictionary Edges"""
     assert map.convert_dict_edges(map._load_data()) != None
     assert map.convert_dict_edges(map._load_data()) != ""
-    assert type(map.convert_dict_edges(map._load_data())) is list
+    is_a(map.convert_dict_edges(map._load_data()), list)
+
 
 def test_convert_dict_nodes():
-    assert map.convert_dict_nodes(map._load_data()) != None
+    """Test Convert Dictionary Nodes"""
+    assert map.convert_dict_nodes(map._load_data()) is not None
     assert map.convert_dict_nodes(map._load_data()) != ""
-    assert map.convert_dict_nodes(map._load_data()) == ['Entrance', 'Boss', 'Cave', 'Armory', 'Gold']
-    assert type(map.convert_dict_nodes(map._load_data())) is list
+    assert map.convert_dict_nodes(map._load_data()) == ['Entrance', 'Boss',
+                                                                'Cave', 'Armory',
+                                                                'Gold']
+    is_a(map.convert_dict_nodes(map._load_data()), list)
+
 
 def test__check_data():
+    """Test Check Data"""
     assert map._check_data(map._load_data()) == True
     assert map._check_data("string") == False
-    assert type(map._check_data(map._load_data())) is bool
+    is_a(map._check_data(map._load_data()), bool)
+
 
 def test_generate_map():
-    assert map._generate_map("", "") is None 
-    assert type(map._generate_map(map.convert_dict_nodes(map._load_data()), map.convert_dict_edges(map._load_data()))) is nx.classes.graph.Graph
-
-
+    """Test Generate Map"""
+    assert map._generate_map("", "") is None
+    is_a(map._generate_map(map.convert_dict_nodes(map._load_data()),
+        map.convert_dict_edges(map._load_data())), nx.classes.graph.Graph)
