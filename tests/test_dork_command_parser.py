2c56830b88dc2c8f872748490c34f2f13a85bcaa
david dews <ddews1@msudenver.edu>
2019-06-19 15:31:13 -0600

    Added some testing for coverage

--- tests/test_dork_command_parser.py
+++ tests/test_dork_command_parser.py
@@ -7,19 +7,19 @@ import tests.daves_mock
 
 def test_hello_world():
     """Testing the hello world function of parser """
-    assert _mp.get_hello_world()[0] == "hello, world!", """Hello world
-     method failed"""
+    assert _mp.get_hello_world() == ("hello, world!", False),\
+         """Hello world method failed"""
 
 
 def test_goodbye_world():
     """Testing the goodbye function of parser """
-    assert _mp.get_goodbye_world()[0] == """goodbye, world!""", """Goodbye
-    world method failed"""
+    assert _mp.get_goodbye_world() == ("""goodbye, world!""", True),\
+         """Goodbye world method failed"""
 
 
 def test_show_help():
     """Testing the help method of parser """
-    assert _mp.get_show_help()[0] == "try typing 'say hello'"
+    assert _mp.get_show_help() == ("try typing 'say hello'", False)
 
 
 def test_player_instance():
@@ -32,32 +32,32 @@ def test_player_instance():
 
 def test_evaluate_hello():
     """Testing the eval function of Repl """
-    assert _mp.evaluate("say hello")[0] == "hello, world!", """Hello world"
-    evalutation failed"""
+    assert _mp.evaluate("say hello") == ("hello, world!", False),\
+         """Hello world evalutation failed"""
 
 
 def test_evaluate_empty():
     """Testing the eval function of Repl """
-    assert _mp.evaluate("")[0] == "Unknown Command", """Input Type Handling"
-    Error, empty string"""
+    assert _mp.evaluate("") == ("Unknown Command", False),\
+         """Input Type Handling Error, empty string"""
 
 
 def test_evaluate_other_type():
     """Testing the eval function of Repl """
-    assert _mp.evaluate(None)[0] == "Unknown Command", """Input Mismatch,"
-    unable to handle"""
+    assert _mp.evaluate(None) == ("Unknown Command", False),\
+         """Input Mismatch, unable to handle"""
 
 
 def test_evaluate_goodbye():
     """Testing the eval function of Repl """
-    assert _mp.evaluate("say goodbye")[0] == "goodbye, world!", """Goodbye world
-    evaluation failed"""
+    assert _mp.evaluate("say goodbye") == ("goodbye, world!", True),\
+    """Goodbye world evaluation failed"""
 
 
 def test_evaluate_help():
     """Testing the eval function of Repl """
-    assert _mp.evaluate("help say")[0] == "try typing 'say hello'", """help say
-    command failed"""
+    assert _mp.evaluate("help say") == ("try typing 'say hello'", False),\
+    """help say command failed"""
 
 
 def test_read_string():
@@ -89,3 +89,20 @@ def test_read_escape_char():
 def test_repl():
     """Testing repl's existance """
     assert isinstance(_mp.repl, object), "Repl doesn't exist"
+
+def test_eval_verb_only():
+    """Testing subject non-existant"""
+    assert _mp.evaluate("say dog") == ("Unknown Command", False),\
+     "verb failed to process"
+
+
+def test_eval_subject_only():
+    """Testing verb non-existant """
+    assert _mp.evaluate("dog help") == ("Unknown Command", False),\
+        "subject failed to process"
+
+
+def test_eval_three_words():
+    """Testing 3 input words"""
+    assert _mp.evaluate("say help help") == ("try typing 'say hello'", False),\
+        "Failed to ignore 3rd word in input"
