8c5371a22561a02bff3c4aeb307b99c27ea2426f
david dews <ddews1@msudenver.edu>
2019-06-18 18:43:05 -0600

    removed them code smells part 1

--- tests/daves_mock.py
+++ tests/daves_mock.py
@@ -1,8 +1,8 @@
 #modified version from Forge and Compiler on Stack Overflow
 class mock_input():
-   arg = ""
+    arg = ""
 
-   def make_input(self,args):
-          return self.arg
-   def change_input(self,args):
-      self.arg = args   
+    def make_input(self):
+        return self.arg
+    def change_input(self, args):
+        self.arg = args
