b268f1c6ba5f50c6621c9ac257d4dc7b6e894740
Larsen Close <closelj@gmail.com>
2019-06-18 21:56:59 -0600

    fixed many pep 8 issues

--- map_parser/map_generator.py
+++ map_parser/map_generator.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Generates map from yaml data
+"""
 
-import networkx as nx 
-import yaml
 from pprint import pprint
+import networkx as nx
+import yaml
 import matplotlib.pyplot as plt
 
+
 CARDINALS = ["north", "east", "south", "west"]
+
 
 edges = []
 nodes = []
@@ -16,31 +21,29 @@ def _load_data(file_name_and_path="./map_parser/yaml/dork.yml"):
 
     return data
 
-def convert_dict_edges(data):
-    edges = []
-   
-    rooms = data["Rooms"]
-    for name in rooms:  
 
+def convert_dict_edges(data):
+    """Convert Dictionary to edges"""
+    edges = []
+    rooms = data["Rooms"]
+    for name in rooms:
         room = rooms[name]
         for direction in CARDINALS:
             if direction in room and room[direction]is not None:
                 edges.append((room[direction], name))
-    
     return edges
 
 def convert_dict_nodes(data):
+    """Convert Dictionary to Nodes"""
     nodes = []
-  
-
     rooms = data["Rooms"]
     for name in rooms:
         nodes.append(name)
         room = rooms[name]
-
     return nodes
 
-def _generate_map(nodes,edges):
+
+def _generate_map(nodes, edges):
     if nodes and edges:
         nx.empty_graph()
         map_graph = nx.Graph()
@@ -48,7 +51,6 @@ def _generate_map(nodes,edges):
         map_graph.add_edges_from(edges)
         # nx.draw(map_graph, with_labels=True)
         plt.show()
-
         return map_graph
 
 # def _check_path():
@@ -66,7 +68,6 @@ def _generate_map(nodes,edges):
 #         print(f"{other} is {direction} of {name}")
 
 def _check_data(data):
-  
     print("\nChecking that 'data' contains a dictionary of rooms... \n")
     if "Rooms" not in data:
         print("No Rooms found.")
@@ -75,22 +76,21 @@ def _check_data(data):
     if not isinstance(data["Rooms"], dict):
         print("Rooms in data was not proper data.")
         return False
-    
+
     print("Rooms found with proper data.\n")
     return True
 
 def main():
+    """Runnable main of map_generator, requires correctly formatted yaml file, returns"""
     data = _load_data()
     print("loaded this data: ")
     pprint(data)
+    print(convert_dict_nodes(_load_data()))
 
     if _check_data(_load_data()):
-            _generate_map(convert_dict_nodes( _load_data()), convert_dict_edges( _load_data()))
-            # _check_path()
-    
-    print(convert_dict_nodes(data))
-    print(convert_dict_edges(data))
+        _generate_map(convert_dict_nodes(_load_data()), convert_dict_edges(_load_data()))
+        # _check_path()
 
-  
+
 if __name__ == "__main__":
     main()
