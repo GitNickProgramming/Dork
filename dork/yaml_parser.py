912f5ed79c927a6ca4498393466401c64f62c2bc
Devon DeJohn <ddejohn@msudenver.edu>
2019-06-20 21:54:01 -0600

    folder restructure and yaml parser rename

--- dork/yaml_parser.py
+++ dork/yaml_parser.py
@@ -7,7 +7,7 @@ __all__ = ["load", "save"]
 def load(file_name):
     """Loads yaml data from the given file_name"""
 
-    file_path = f"./dork/dork_utils/yaml/{file_name}.yml"
+    file_path = f"./dork/yaml/{file_name}.yml"
     with open(file_path) as file:
         data = yaml.safe_load(file.read())
     return data
