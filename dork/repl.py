933cddf6cfee4693a69269c1667249b021e0adb3
Devon DeJohn <ddejohn@msudenver.edu>
2019-06-26 14:47:04 -0600

    removed globals

--- dork/repl.py
+++ dork/repl.py
@@ -3,44 +3,46 @@
 import dork.game_utils.game_data as game_data
 
 
-REPL_INSTANCE = game_data.REPL()
-TITLE = game_data.TITLE
-CMDS = game_data.CMDS
-MOVES = game_data.MOVES
-ERRS = game_data.ERRS
-META = game_data.META
-
-
 def read():
     """get input from CLI"""
     return str.casefold(input("> "))
 
 
-def evaluate(cmd):
+def evaluate(cmd, repl_instance, repl_data):
     """parse a cmd and run it"""
+    cmds, moves, meta, errs = repl_data
     cmd = cmd.split()
     if cmd:
         noun, verb = cmd.pop() if len(cmd) > 1 else None, cmd.pop()
-        instruction = CMDS.get(verb, MOVES.get(verb, META.get(verb, ERRS["u"])))
+        instruction = cmds.get(verb, moves.get(verb, meta.get(verb, errs["u"])))
         if isinstance(instruction, dict):
-            instruction = instruction.get(noun, ERRS["u"])
+            instruction = instruction.get(noun, errs["u"])
     else:
-        instruction = ERRS["?"]
+        instruction = errs["?"]
 
     method = instruction[0]
     arg = instruction[1] if len(instruction) > 1 else None
 
     if not arg:
-        return getattr(REPL_INSTANCE, method)()
-    return getattr(REPL_INSTANCE, method)(arg)
+        return getattr(repl_instance, method)()
+    return getattr(repl_instance, method)(arg)
 
 
 def repl():
     """Read eval print loop
     """
-    print(f"\n\nGreetings, {REPL_INSTANCE.name}! " + TITLE + "\n\n")
+    repl_instance = game_data.REPL()
+    repl_data = (
+        game_data.CMDS,
+        game_data.MOVES,
+        game_data.META,
+        game_data.ERRS
+    )
+    print(f"\n\nGreetings, {repl_instance.name}! " + game_data.TITLE + "\n\n")
     while True:
-        output, should_exit = evaluate(read())
+        output, should_exit = evaluate(
+            cmd=read(), repl_instance=repl_instance, repl_data=repl_data
+        )
         print(output)
         if should_exit:
             break
