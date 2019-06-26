36f4f97c424b1d10b1b3769f64252f36a9d48f92
Devon DeJohn <ddejohn@msudenver.edu>
2019-06-25 20:00:29 -0600

    better doc string

--- dork/game_utils/yaml_parser.py
+++ dork/game_utils/yaml_parser.py
@@ -6,9 +6,14 @@ __all__ = ["load"]
 
 
 def load(file_name):
-    """Loads yaml data from the given file_name"""
+    """Loads yaml data from the given file_name.
+    \nArguments:
+        file_name {String} -- [format: folder_name/file_name]
+    \nReturns:
+        {dict} -- [Returns a dictionary object holding YAML data]
+    """
 
-    file_path = f"./yaml/{file_name}.yml"
+    file_path = f"./{file_name}.yml"
     with open(file_path) as file:
         data = yaml.safe_load(file.read())
     return data
