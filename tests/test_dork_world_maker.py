6367a08d4a1bf623c195d254aa0fbe7dc2821b12
Devon DeJohn <ddejohn@msudenver.edu>
2019-06-30 12:52:15 -0600

    fleshed out world loading tests

---
+++ tests/test_dork_world_maker.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Basic tests for world loader
+"""
+import dork.game_utils.world_maker as world_maker
+from tests.utils import is_a
+
+
+def test_world_loader():
+    """world_maker.load_game should return a dict
+    """
+    nobody = world_maker.load_game("nobody")
+    is_a(nobody, dict)
+
+    keys = ["rooms", "players"]
+    for key in keys:
+        assert key in nobody.keys()
+
+    rooms = [
+        "boss",
+        "gold",
+        "armory",
+        "cave",
+        "rock ledge",
+        "entrance"
+    ]
+    for room in nobody["rooms"]:
+        assert nobody["rooms"][room]["name"] in rooms
+
+    players = [
+        "troll",
+        "ghost",
+        "hero"
+    ]
+    for player in nobody["players"]:
+        assert player in players
+
+
+# Mock to get the successful load message
+def test_load_existing_file():
+    """world_maker.load_game should successfully load a save file
+    """
+    bobby_b = world_maker.load_game("bobby b")
+    is_a(bobby_b, dict)
+    
+    keys = ["rooms", "players"]
+    for key in keys:
+        assert key in bobby_b.keys()
+
+    rooms = [
+        "boss",
+        "gold",
+        "armory",
+        "cave",
+        "rock ledge",
+        "entrance"
+    ]
+    for room in bobby_b["rooms"]:
+        assert bobby_b["rooms"][room]["name"] in rooms
+
+    players = [
+        "troll",
+        "ghost",
+        "hero"
+    ]
+    for player in bobby_b["players"]:
+        assert player in players
