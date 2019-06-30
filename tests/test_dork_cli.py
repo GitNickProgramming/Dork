a9a7a7665fbb1502c4f5596b9cb37bd35d970517
Devon DeJohn <ddejohn@msudenver.edu>
2019-06-29 22:32:40 -0600

    added CLI help test back

--- tests/test_dork_cli.py
+++ tests/test_dork_cli.py
@@ -10,3 +10,12 @@ def test_cli_exists():
     """
     assert "main" in vars(dork.cli), "Dork.cli should define a main method"
     assert isinstance(dork.cli.main, FunctionType)
+
+
+def test_cli_help(run):
+    """CLI's help command should return helpful information
+    """
+    out, err, mocked_input = run(dork.cli.main, "-h")
+    assert "usage:" in out
+    assert err == ""
+    assert mocked_input.call_count == 0
