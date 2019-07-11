1caa908b35eff564958d5844a02cf39f4d9ced33
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-10 19:48:27 -0600

    BYE FELICIA

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,69 +1,9 @@
 # -*- coding: utf-8 -*-
 """Basic tests for state and entity relationships in dork
 """
-
+# pylint: disable=protected-access
 from tests.utils import has_a, has_method, is_a
 import dork.types as types
-
-
-# pylint: disable=protected-access
-
-
-def test_game_attributes(game):
-    """the game should have attributes
-    """
-    has_a(game, "players")
-    has_a(game, "worldmap")
-    has_a(game, "hero")
-    has_method(game, "build")
-    has_method(game, "_build_players")
-    has_method(game, "_build_world")
-    has_method(game, "_build_hero")
-    has_method(game, "_gtfo")
-    has_method(game, "_move")
-    has_method(game, "_inventory")
-    has_method(game, "_look")
-
-
-def test_room_attributes(room):
-    """the room should have attributes
-    """
-    has_a(room, "name")
-    has_a(room, "adjacent")
-    has_a(room, "description")
-    has_a(room, "players")
-    has_a(room, "items")
-    has_a(room, "clues")
-
-
-def test_player_attributes(player):
-    """the player should have attributes
-    """
-    has_a(player, "name")
-    has_a(player, "location")
-    has_a(player, "items")
-    has_a(player, "equipped")
-
-
-def test_item_attributes(item):
-    """item should have holder and capacity
-    """
-    has_a(item, "name")
-    has_a(item, "description")
-    has_a(item, "stats")
-
-
-def test_holder_attributes(holder):
-    """holder should have items
-    """
-    has_a(holder, "items")
-
-
-def test_worldmap_attributes(worldmap):
-    """the worldmap should have a dict of rooms
-    """
-    has_a(worldmap, "rooms")
-    has_a(worldmap, "players")
 
 
 def test_confirm_method_yes(capsys, mocker):
