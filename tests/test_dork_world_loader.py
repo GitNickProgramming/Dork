170610be0dd70b5ea8655e481f033e4f6e3d3b59
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-17 12:21:06 -0600

    heck off

--- tests/test_dork_world_loader.py
+++ tests/test_dork_world_loader.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-"""Basic tests for world loader
-"""
+"""Basic tests for world loader"""
+
 from dork.game_utils.world_loader import main as load
-from tests.utils import is_a
 
 
 def test_world_loader():
+    """load an existing save, and one that doesn't exist"""
+
     assert isinstance(load("bobby b"), dict)
     assert isinstance(load("awigahwgh"), dict)
 
 
 def test_maze_generator(maze):
-    """run the maze generator
-    """
+    """run the maze generator"""
+
     maze.draw()
     maze.update()
