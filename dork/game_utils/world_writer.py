6f45ee12f27c62521a0f9a625a821b6369469f74
Zach <zachoryanguiano@gmail.com>
2019-07-17 10:20:15 -0600

    whitespace error

--- dork/game_utils/world_writer.py
+++ dork/game_utils/world_writer.py
@@ -29,7 +29,8 @@ def save_gamee(game):
                     continue
                 data["players"]["hero"]["inventory"][number]\
                     ["name"] = player_inventory[item].name
-                data["players"]["hero"]["inventory"][number]["description"] = player_inventory[item].description
+                data["players"]["hero"]["inventory"][number]\
+                    ["description"] = player_inventory[item].description
                 data["players"]["hero"]["inventory"][number]\
                     ["stats"] = player_inventory[item].stats
             if number < len(player_inventory)-1:
