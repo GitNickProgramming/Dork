2e9825b52f918b8fb9c9d597a92c4aff9ddc5e5d
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-17 12:38:02 -0600

    added tests

--- tests/test_dork_repl.py
+++ tests/test_dork_repl.py
@@ -41,48 +41,17 @@ def test_repl_evaluate(game, repl_data):
     assert repl.evaluate("walk map", game, repl_data) == (
         "You can't go that way", False)
 
-    assert "You have entered" or "You cannot go" in repl.evaluate(
-        "N", game, repl_data)
+    assert "dummy description" or "You cannot go" in repl.evaluate(
+        "n", game, repl_data)
 
-    assert "You have entered" or "You cannot go" in repl.evaluate(
+    assert "dummy description" or "You cannot go" in repl.evaluate(
+        "s", game, repl_data)
+
+    assert "dummy description" or "You cannot go" in repl.evaluate(
+        "e", game, repl_data)
+
+    assert "dummy description" or "You cannot go" in repl.evaluate(
+        "w", game, repl_data)
+
+    assert "dummy description" or "You cannot go" in repl.evaluate(
         "walk south", game, repl_data)
-
-
-# def test_repl_evaluate_various_functions(game, repl_data):
-#     """Dork.repl.evaluate has various functions
-#     """
-
-#     assert repl.evaluate(".rq", game, repl_data) == (
-#         'Thanks for playing DORK, None!', True)
-
-#     assert repl.evaluate(".z", game, repl_data) == (
-#         "Oh shit, you found an easter egg!", False)
-
-#     assert "dummy description" in repl.evaluate("look", game, repl_data)[0]
-
-#     assert repl.evaluate("i", game, repl_data) == (
-#         "    You ain't got shit, son!", False)
-
-
-# def test_repl_evaluate_move(game, repl_data):
-#     """Testing simple repl functions
-#     """
-#     assert repl.evaluate(".rq", game, repl_data) == (
-#         'Thanks for playing DORK, None!', True)
-#     assert repl.evaluate(".z", game, repl_data) == (
-#         "Oh shit, you found an easter egg!", False)
-
-
-# def test_repl_method_repl(run, mocker):
-#     """Dork.repl.repl should output like this then end in these conditions
-#     """
-#     evaluate_values = [("FooBar", False), ("Bufarr", True)]
-#     mocked_evaluate = mocker.patch("evaluate")
-#     mocked_evaluate.side_effect = evaluate_values
-
-#     out, err, mocked_input = run(
-#         repl.repl, input_return_value="42")
-#     assert "FooBar" in out
-#     assert "Bufarr" in out
-#     assert err == ""
-#     assert mocked_input.call_count == len(evaluate_values) + 1
