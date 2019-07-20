ab7ccf85e3ef35a93bd8427a2b71baa4d2d8c6c0
Devon DeJohn <ddejohn@msudenver.edu>
2019-07-20 14:07:10 -0600

    fixed tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -6,62 +6,61 @@ from tests.utils import is_a
 import dork.types as types
 
 
-# def test_confirm_method_yes(capsys, mocker):
-#     """confirm should do things
-#     """
-#     mocked_input = mocker.patch('builtins.input')
-#     mocked_input.side_effect = ["y"]
-#     assert types.Game._confirm()
-#     captured = capsys.readouterr()
-#     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-#     assert mocked_input.call_count == 1
+def test_confirm_method_yes(capsys, mocker):
+    """confirm should do things
+    """
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = ["y", "tester"]
+    assert types.Game._confirm()
+    captured = capsys.readouterr()
+    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
+    assert mocked_input.call_count == 1
 
 
-# def test_confirm_method_no(capsys, mocker):
-#     """confirm should do things
-#     """
-#     mocked_input = mocker.patch('builtins.input')
-#     mocked_input.side_effect = ["n"]
-#     assert types.Game._confirm() is False
-#     captured = capsys.readouterr()
-#     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-#     assert mocked_input.call_count == 1
+def test_confirm_method_no(capsys, mocker):
+    """confirm should do things
+    """
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = ["n"]
+    assert types.Game._confirm() is False
+    captured = capsys.readouterr()
+    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
+    assert mocked_input.call_count == 1
 
 
-# def test_confirm_method_blank(capsys, mocker):
-#     """confirm should do things
-#     """
-#     mocked_input = mocker.patch('builtins.input')
-#     mocked_input.side_effect = ["afk", "    ", "y"]
-#     types.Game._confirm()
-#     captured = capsys.readouterr()
-#     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-#     assert "That is not a valid response!" in captured.out
-#     assert mocked_input.call_count == 3
+def test_confirm_method_blank(capsys, mocker):
+    """confirm should do things
+    """
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = ["afk", "    ", "y", "tester"]
+    types.Game._confirm()
+    captured = capsys.readouterr()
+    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
+    assert "That is not a valid response!" in captured.out
+    assert mocked_input.call_count == 3
 
 
-# def test_start_over_no(capsys, mocker, game):
-#     """confirm should do things
-#     """
-#     mocked_input = mocker.patch('builtins.input')
-#     mocked_input.side_effect = ["n"]
-#     assert game._start_over("the notification string") == (
-#         "Guess you changed your mind!", False)
-#     captured = capsys.readouterr()
-#     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-#     assert mocked_input.call_count == 1
+def test_start_over_no(capsys, mocker, game):
+    """confirm should do things
+    """
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = ["n", ".rq"]
+    assert game._start_over() == ("Guess you changed your mind!", False)
+    captured = capsys.readouterr()
+    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
+    assert mocked_input.call_count == 1
 
 
-# def test_start_over_yes(capsys, mocker, game):
-#     """confirm should do things
-#     """
-#     # the call count here as 2 is a magic number need to document that
-#     mocked_input = mocker.patch('builtins.input')
-#     mocked_input.side_effect = ["y", "new player name", ".rq"]
-#     game._start_over("the notification string")
-#     captured = capsys.readouterr()
-#     assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
-#     assert mocked_input.call_count == 2
+def test_start_over_yes(capsys, mocker, game):
+    """confirm should do things
+    """
+    # the call count here as 2 is a magic number need to document that
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = ["y", "tester"]
+    game._start_over()
+    captured = capsys.readouterr()
+    assert "\n!!!WARNING!!! You will lose unsaved data!\n" in captured.out
+    assert mocked_input.call_count == 2
 
 
 def test_player_location(game, player):
