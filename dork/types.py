90fbfdf992ec51a2544c4b0dce6d300c3f62d93c
zanguian <zanguian@msudenver.edu>
2019-07-26 23:13:00 -0600

    added whitespace

--- dork/types.py
+++ dork/types.py
@@ -612,7 +612,7 @@ class Game:
         num = len(inv_list)
         description = location.description.splitlines()
         des = location.description
-        if num >1:
+        if num > 1:
             rand_ind = randrange(4)
             des = description[0] + "\n" + description[1] + "\n" \
                 + factory_data.ROOM_INV_DESCRIPTIONS["1"][rand_ind]
